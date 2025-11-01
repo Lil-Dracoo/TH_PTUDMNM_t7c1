1acfaee2b2b92cbe731af900eae2a995c43c16f5
DuCuong <ducuong133@gmail.com>
2025-11-01 08:04:35 +0700

    b34

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+<<<<<<< HEAD
 
 int tong(int a, int b){
 	return a+b;
@@ -13,11 +14,19 @@ int tich(int a, int b){
 	return a*b;
 }
 
+=======
+int tong(int a, int b){
+	return a+b;
+}
+int hieu(int a, int b);
+int tich(int a, int b);
+>>>>>>> 0ed6472 (tong)
 float thuong(int a, int b);
 int main()
 {
 	int a, int b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+<<<<<<< HEAD
 	cout<<"Nhap a: ";
 	cin>>a;
 	cout<<"Nhap b: ";
@@ -28,6 +37,9 @@ int main()
 	cout<<endl;
 	cout<<"Tam2";
 
+=======
+	cout<<"Tong = "<<tong(3,1)<<endl;
+>>>>>>> 0ed6472 (tong)
 	system("pause");
 	return 0;
 }
